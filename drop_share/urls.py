8dead78e6a8ef6185aab39ebf471a4e811402fca
MBissett29 <mbissett@hotmail.com>
2022-02-03 10:38:05 +0000

    Add all auth to settings and urls into drop_share

--- drop_share/urls.py
+++ drop_share/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('DS_blog.urls'), name='DS_blog.urls'),
+    path('accounts/', include('allauth.urls')),
 ]
