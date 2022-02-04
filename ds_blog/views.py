0181a750ca4c69ec6ad7c20e8bf81347daec2f19
MBissett29 <mbissett@hotmail.com>
2022-02-04 21:24:57 +0000

    Add comment form to the post detail view

--- ds_blog/views.py
+++ ds_blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, View, CreateView
-from .models import Post
+from .models import Post, Comment
 from .forms import UserPostForm, CommentForm
 from django.http import HttpResponseRedirect
 from django.utils.text import slugify
@@ -31,11 +31,44 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
+                "commented": False,
                 "liked": liked,
                 "comment_form": CommentForm()
             },
         )
 
+    def post(self, request, slug, *args, **kwargs):
+        """This gets the combines the comments and blog post and likes together"""
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('-created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+
+        return render(
+            request,
+            "user_post.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
 
 
 class UserPost_Create(CreateView):
