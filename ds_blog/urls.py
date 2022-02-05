7a80e9105244b41dc3bf63e35707aa740de40b72
MBissett29 <mbissett@hotmail.com>
2022-02-05 01:24:06 +0000

    Create the url to see the post being liked

--- ds_blog/urls.py
+++ ds_blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PostList, PostDetail, UserPost_Create
+from .views import PostList, PostDetail, UserPost_Create, PostLike
 
 urlpatterns = [
     path('', PostList.as_view(), name='home'),
     path('<slug:slug>/', PostDetail.as_view(), name='user_post'),
     path('post_blog', UserPost_Create.as_view(), name='post_blog'),
+    path('like/<slug:slug>', PostLike.as_view(), name='post_like'),
 ]
